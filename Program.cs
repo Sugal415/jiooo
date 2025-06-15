99eb75a3c1ee38ef216782c62205d9cc1fc34c1c
Sugal415 <sugaldmgr@gmail.com>
2025-06-15 08:26:33 +0545

    Add id feature

---
+++ Program.cs
@@ -0,0 +1,18 @@
+ using System;
+
+namespace SoulRingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("🌟 Welcome to Soul Ring Quest! 🌟");
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter user id: ");
+            string id = Console.ReadLine();
+        }
+    }
+}
